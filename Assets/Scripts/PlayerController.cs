4789a2037780bd81b81cb369607060d9a77a3277
Toby J <toby.johnson19@education.nsw.gov.au>
2026-02-11 10:32:08 +1100

    WIP: Added collectibles, score, walls, win conditions as well as a beginning on an enemy AI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
-
-
     private Rigidbody rb; 
+    private int count;
     private float movementX;
     private float movementY;
+
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
      
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      rb = GetComponent <Rigidbody>();   
+        rb = GetComponent<Rigidbody>();  
+        count = 0;
+        SetCountText(); 
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -29,6 +35,27 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }  
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        
     }
+
+        void SetCountText() 
+    {
+       countText.text =  "Count: " + count.ToString();
+       if (count >= 12)
+       {
+            winTextObject.SetActive(true);
+       }
+    }
+
 
 }
