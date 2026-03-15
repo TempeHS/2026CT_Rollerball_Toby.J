8ee4ea88da625439c2ec4645befba2bbe1ff642d
Toby J <toby.johnson19@education.nsw.gov.au>
2026-03-16 09:40:23 +1100

    WIP :Obstacle course

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    public Transform player;
+    public Transform orientation;
+    public float rotationSpeed = 5f;
+    public Vector3 cameraOffset = new Vector3(0, 2, -5);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        offset = transform.position - player.transform.position;
-    }
+
 
     // Update is called once per frame
+    }
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.position + orientation.TransformDirection(cameraOffset);
+        transform.LookAt(player.position);
     }
 }
